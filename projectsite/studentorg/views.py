a4d17c5e8ff0f19469598e577c73891e55ce00e5
Nicole Cabral <cabralnicolef@gmail.com>
2026-09-06 11:12:04 +0800

    search feature, dashboard, & sorting

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -11,16 +11,39 @@ from studentorg.forms import (
 )
 from django.urls import reverse_lazy
 
+from django.utils import timezone
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
+
+from django.db.models import Q
 
 class OrganizationList(ListView):
     model = Organization
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    ordering = ["college__college_name","name"]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -124,6 +147,13 @@ class ProgramList(ListView):
     context_object_name = 'program'
     template_name = 'program_list.html'
     paginate_by = 5
+
+    def get_ordering(self):
+        allowed = ["prog_name", "college__college_name"]
+        sort_by = self.request.GET.get("sort_by")
+        if sort_by in allowed:
+            return sort_by
+        return "prog_name"
 
 class ProgramCreateView(CreateView):
     model = Program
