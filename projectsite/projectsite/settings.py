9d1f2d469ffb136e93bbb1e8cc0ffe1b8d4a9ada
Nicole Cabral <cabralnicolef@gmail.com>
2026-09-09 08:48:20 +0800

    fixed logout link

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -152,7 +152,7 @@ LOGIN_URL = '/accounts/login/'            # where @login_required will send user
 LOGIN_REDIRECT_URL = '/'                  # where to go after successful login
 LOGOUT_REDIRECT_URL = '/accounts/login/'  # after logout, go back to login
 
-ACCOUNT_LOGOUT_REDIRECT_URL = '/'         # where to redirect after logout
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'  # where to redirect after logout
 ACCOUNT_LOGOUT_ON_GET = True              # logout immediately on GET
 
 ACCOUNT_LOGIN_METHODS = {"username", "email"} # allow login with username OR email
